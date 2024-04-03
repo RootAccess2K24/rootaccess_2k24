28dc7acfb9bf77cff1ec7bb9b498a25394b817e4
blackjack150 <119121149+blackjack150@users.noreply.github.com>
2024-04-03 18:57:39 +0530

    Add files via upload

---
+++ Crypto/safe crack-1/public/safecracker.py
@@ -0,0 +1,17 @@
+import base64
+
+def password_checker(password, encoded_key):
+    decoded_key = base64.b64decode(encoded_key).decode('utf-8')
+    if password == decoded_key:
+        return True
+    else:
+        return False
+
+# Example usage:
+encoded_key = "cGFzc3dvcmQxMjNfc3dhZw=="
+password = input("Enter password: ")
+
+if password_checker(password, encoded_key):
+    print("Password is correct!")
+else:
+    print("Incorrect password!")
